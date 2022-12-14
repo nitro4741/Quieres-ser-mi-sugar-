747e1ee0d049f9969146c81cffd3d750fa003ab6
Diego Ramirez <die35go45@gmail.com>
2022-12-13 23:03:13 -0500

    preguntar name

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const documenReady = () => {
+   const heroTitle = document.getElementById('heroTitle');
    const yesButton = document.getElementById('yesButton');
    const noButton = document.getElementById('noButton');
 
@@ -12,14 +13,17 @@ const documenReady = () => {
     alert('Te amo mucho señora de mi corazon, feliz dia porq todos los dias que te lo dije tarde');
     location.href = 'https://www.youtube.com/watch?v=xfDiy0va_ig';
     
-   }
+   };
 
 
 
    const sad = () => {
     noButton.style.top = Math.random() * innerHeight + 'px';
     noButton.style.left = Math.random() * innerWidth + 'px';
-   }
+   };
+
+   const partner = prompt('Dime tu name ❤️')
+   heroTitle.innerText += ` ${partner || 'BB'} ? `; 
 
    yesButton.addEventListener('click', getPartner ) 
    noButton.addEventListener('mouseover', sad)
